182592d56b70affbe60b87de9d72c56c9e8c2910
VoMinh88 <vhm.autotek@gmail.com>
2023-09-12 23:19:01 +0700

    comit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -16,91 +16,87 @@ function Navbar() {
     dispatch(getCates());
   },[])
 
-return (
-  <div>
-    <nav
-      className="navbar navbar-expand-lg bg-body-tertiary"
-      data-bs-theme="dark"
-    >
-      <div className="container-fluid">
-        <Link className="navbar-brand" href="#">
-          Navbar
-        </Link>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-            <li className="nav-item">
-              <Link className="nav-link active" aria-current="page" href="#">
-                Home
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" href="#">
-                Link
-              </Link>
-            </li>
-            <li className="nav-item dropdown">
-              <Link
-                className="nav-link dropdown-toggle"
-                href="#"
-                role="button"
-                data-bs-toggle="dropdown"
-                aria-expanded="false"
-              >
-                Dropdown
-              </Link>
-              <ul className="dropdown-menu">
-                <li>
-                  <Link className="dropdown-item" href={"/todo"}>
-                    Action
-                  </Link>
-                </li>
-                <li>
-                  <Link className="dropdown-item" href="#">
-                    Another action
-                  </Link>
-                </li>
-                <li>
-                  <hr className="dropdown-divider" />
-                </li>
-                <li>
-                  <Link className="dropdown-item" href="#">
-                    Something else here
-                  </Link>
-                </li>
-              </ul>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link loginNar" href={"/login"} aria-disabled="true">
-                Login
-              </Link>
-            </li>
-          </ul>
-          <form className="d-flex" role="search">
-            <input
-              className="form-control me-2"
-              type="search"
-              placeholder="Search"
-              aria-label="Search"
-            />
-            <button className="btn btn-outline-success" type="submit">
-              Search
-            </button>
-          </form>
-        </div>
-      </div>
-    </nav>
-  </div>
-);
+// return (
+//   <div>
+//     <nav
+//       className="navbar navbar-expand-lg bg-body-tertiary"
+//       data-bs-theme="dark"
+//     >
+//       <div className="container-fluid">
+//         <Link className="navbar-brand" href="#">
+//           Navbar
+//         </Link>
+//         <button
+//           className="navbar-toggler"
+//           type="button"
+//           data-bs-toggle="collapse"
+//           data-bs-target="#navbarSupportedContent"
+//           aria-controls="navbarSupportedContent"
+//           aria-expanded="false"
+//           aria-label="Toggle navigation"
+//         >
+//           <span className="navbar-toggler-icon"></span>
+//         </button>
+//         <div className="collapse navbar-collapse" id="navbarSupportedContent">
+//           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+//             <li className="nav-item">
+//               <Link className="nav-link active" aria-current="page" href={"/home"}>
+//                 Home
+//               </Link>
+//             </li>
+//             <li className="nav-item">
+//               <Link className="nav-link" href="#">
+//                 Link
+//               </Link>
+//             </li>
+//             <li className="nav-item dropdown">
+//               <Link
+//                 className="nav-link dropdown-toggle"
+//                 href="#"
+//                 role="button"
+//                 data-bs-toggle="dropdown"
+//                 aria-expanded="false"
+//               >
+                
+//               </Link>
+//               <ul className="dropdown-menu">
+//                 <li>
+//                   <Link className="dropdown-item" href={"#"}>
+//                     Sản Phẩm
+//                   </Link>
+//                 </li>
+//                 {cates.map((job) => (
+//                 <li> 
+//                   <Link className="dropdown-item" key={cates.id}>
+//                   {cates.name}
+//                   </Link>
+//                 </li>
+//               ))}
+
+                
+//               </ul>
+//             </li>
+//             <li className="nav-item">
+//               <Link className="nav-link loginNar" href={"/login"} aria-disabled="true">
+//                 Login
+//               </Link>
+//             </li>
+//           </ul>
+//           <form className="d-flex" role="search">
+//             <input
+//               className="form-control me-2"
+//               type="search"
+//               placeholder="Search"
+//               aria-label="Search"
+//             />
+//             <button className="btn btn-outline-success" type="submit">
+//               Search
+//             </button>
+//           </form>
+//         </div>
+//       </div>
+//     </nav>
+//   </div>
+// );
 }
 export default Navbar;
